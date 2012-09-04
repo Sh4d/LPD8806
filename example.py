66b7728f03bbf4a5525d552c8c8e5575b25ff299
Brad Beattie <brad@gossamer-threads.com>
2012-09-04 15:14:08 -0700

    Placating pyflakes *.py; pep8 *.py;

--- example.py
+++ example.py
@@ -6,17 +6,17 @@ import LPD8806
 led = LPD8806.strand()
 
 while True:
-	for i in range(5):
-		led.fill(255, 0, 0)
-		led.update()
-		sleep(0.3)
-		led.fill(0, 255, 0)
-		led.update()
-		sleep(0.3)
-		led.fill(0, 0, 255)
-		led.update()
-		sleep(0.3)
+    for i in range(5):
+        led.fill(255, 0, 0)
+        led.update()
+        sleep(0.3)
+        led.fill(0, 255, 0)
+        led.update()
+        sleep(0.3)
+        led.fill(0, 0, 255)
+        led.update()
+        sleep(0.3)
 
-	for i in range(300):
-		led.wheel()
-		led.update()
+    for i in range(300):
+        led.wheel()
+        led.update()
